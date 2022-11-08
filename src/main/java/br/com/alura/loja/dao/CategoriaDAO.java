e1571ea77b1046565b88509dee18282faee31ace
Mona Lisa <gabriel.2000mourad@gmail.com>
2022-11-08 19:41:21 -0300

    Mapeando uma entidade com relações N para 1.

---
+++ src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.loja.model.Categoria;
+import br.com.alura.loja.model.Produto;
+
+import javax.persistence.EntityManager;
+
+public class CategoriaDAO {
+
+    private EntityManager entityManager;
+
+    public CategoriaDAO (EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void salvar(Categoria categoria) {
+        this.entityManager.persist(categoria);
+    }
+}
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.model.Categoria;
 import br.com.alura.loja.model.Produto;
@@ -12,13 +13,16 @@ import java.math.BigDecimal;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
-        Produto celular = new Produto("Xiamo RedMI Note","Topzera",new BigDecimal(1200), Categoria.CELULARES);
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiamo RedMI Note","Topzera",new BigDecimal(1200), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
         em.getTransaction().begin();
 
         ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
+        categoriaDAO.salvar(celulares);
         produtoDAO.salvar(celular);
         em.getTransaction().commit();
         em.close();
